46ea18dd16c3f85d29936f3fa4f79b4ab3a10c79
bogatovad <ArtembBogatov@yandex.ru>
2021-06-16 10:23:24 +0300

    test

--- grocery_assistant/settings.py
+++ grocery_assistant/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'grocery_assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'dbserv',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 } 
 
